2e85f4f3fc467402a73cef99b63fe907aea5f396
jeovane lima <145929373+JeovaneSilva@users.noreply.github.com>
2023-10-28 21:59:08 -0300

    finalizando e atualizando o projeto

---
+++ script.js
@@ -0,0 +1,30 @@
+function abrirModal() {
+    const header = document.querySelector('header')
+    const footer = document.querySelector('footer')
+    const slidercontainer = document.querySelector('main')
+    const destaques = document.querySelector('#featured-container')
+    const modal = document.querySelector('.janela-modal')
+    modal.classList.add('abrir')
+
+    header.style.display = "none"
+    footer.style.display = "none"
+    slidercontainer.style.display = "none"
+    destaques.style.display = "none"
+
+   
+
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == 'fechar') {
+            modal.classList.remove('abrir')
+
+            header.style.display = "block"
+            footer.style.display = "block"
+            slidercontainer.style.display = "block"
+            destaques.style.display = "block"
+        }
+    })
+}
+
+function enviarEmail() {
+    window.alert('Obrigado pela sua mensagem, aguarde um momento!')
+}
